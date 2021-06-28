28c6f728267c7ad37450297a8aa5ebfb4c46d763
István Derda <isde@spreadshirt.net>
2021-06-28 13:49:31 +0200

    update not visible definition

--- src/components/NotVisibleDefinition.svelte
+++ src/components/NotVisibleDefinition.svelte
@@ -1,6 +1,5 @@
 <b>(Partially) Not Visible</b>:
 <ul>
     <li>The design does <b>not</b> elevate itself clearly from the background.</li>
-    <li>Either all of the Design is not or hardly visible on the background-color.</li>
-    <li>OR parts of the design are hard to make out or lost on the background-color.</li>
+    <li>Either all of the Design or parts are not not visible or hard to make out on the background-color.</li>
 </ul>
