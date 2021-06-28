35bc9a463a3a21da2e1c6eda02932710f3744ff2
István Derda <isde@spreadshirt.net>
2021-06-28 10:46:53 +0200

    add content to info-modal

--- src/components/InfoModal.svelte
+++ src/components/InfoModal.svelte
@@ -1,4 +1,34 @@
-
-
-<h1>Welcome to visi<b>lable</b>!</h1>
+<h1>Welcome to Visi<b>lable</b>!</h1>
 Thank you for helping me out!
+<br/><br/>
+Please Read this introduction carefully, before starting to use this tool.
+<h2>What is this about?</h2>
+<p>
+    I'm currently working on my bachelors-thesis in computer-science with the title "Rating the visibility of digital images with transparencies on plain backgrounds".
+    <br/>
+    <i>Visi<b>lable</b></i> is a Tool I built to gather Data on what Humans perceive as "visible". <br/>
+    The Data will be used to evaluate my success in implementing an algorithm classifying designs as "Visible" or "(Partially) Not Visible".
+</p>
+<h2>What do I mean by "(Partially) Not Visible" and "Visible"</h2>
+<p>
+    The question is <b>not</b> about aesthetics or visual appeal.
+</p>
+<section>
+    <b>Visible</b>:
+<ul>
+    <li>The design elevates itself from the background.</li>
+    <li>AND No detail is lost or hard to make out because of the background-color.</li>
+</ul>
+<b>(Partially) Not Visible</b>:
+<ul>
+    <li>All of the Design is not or hardly visible on the background-color.</li>
+    <li>OR Parts of the design are hard to make out or lost on the background-color.</li>
+</ul>
+</section>
+<h2>How to use this Tool:</h2>
+<ul>
+    <li>Drag&Drop the designs into the classes <i>"Visible"</i> and <i>"(Partially) Not Visible"</i></li>
+    <li>You can click a design to open it in a bigger view.</li>
+    <li>The buttons on the left let you rate all designs left in the <i>"To Check"</i>-field at once.</li>
+    <li>Click <i>"NEXT"</i> when you rated all designs.</li>
+</ul>
