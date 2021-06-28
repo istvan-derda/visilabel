fcade484fa989c01aef00ee8b108d7d77159ab03
István Derda <isde@spreadshirt.net>
2021-06-28 13:47:48 +0200

    longer end-modal

--- src/components/EndModal.svelte
+++ src/components/EndModal.svelte
@@ -1,5 +1,12 @@
 <div>
-    Congratulations, that's all! 🎉
-    <br/><br/>
+    <h1>
+        Congratulations, that's all! 🎉
+    </h1>
+    <p>
+        You have seen and rated every design at least once. That is amazing!
+    </p>
+    <p>
+        You are welcome to reload the page and rate the designs on other background-colors. But you earned yourself a break.
+    </p>
     Thank you for your time.
 </div>
