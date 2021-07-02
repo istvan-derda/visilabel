404db63dceac830438678765d34ce1f67433f609
István Derda <isde@spreadshirt.net>
2021-07-02 15:01:41 +0200

    update visible definition

--- src/components/VisibleDefinition.svelte
+++ src/components/VisibleDefinition.svelte
@@ -1,6 +1,7 @@
 <b>Visible</b>:
 <ul>
-    <li>Visible enough to be shown in Marketplace-search and to be printed.</li>
+    <li>The design is be easily recognizable.</li>
+    <li>Should be visible enough to be printed or shown in a search.</li>
     <li>No detail is lost.</li>
-    <li>No areas of the design merging with the background.</li>
+    <li>All text is readable.</li>
 </ul>
