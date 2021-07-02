0c29052b6a3b96afa8b25cc25faea1027e11e0e1
István Derda <isde@spreadshirt.net>
2021-07-02 16:52:14 +0200

    fix typo

--- src/components/VisibleDefinition.svelte
+++ src/components/VisibleDefinition.svelte
@@ -1,8 +1,8 @@
 <b>Visible</b>:
 <ul>
-    <li>The design is be easily recognizable.</li>
+    <li>The design is easily recognizable.</li>
     <li>Should be visible enough to be printed or shown in a search.</li>
-    <li>No detail is lost.</li>
-    <li>All text is readable.</li>
+    <li>No detail is lost or hard to see.</li>
+    <li>All text is readable with ease.</li>
     <li>Be strikt!</li>
 </ul>
