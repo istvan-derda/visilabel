1c8cbab021c783a3a0494b5d96213d802619b1ed
István Derda <isde@spreadshirt.net>
2021-07-08 17:55:00 +0200

    tweak definitions

--- src/components/infoText/VisibleDefinition.svelte
+++ src/components/infoText/VisibleDefinition.svelte
@@ -1,6 +1,6 @@
 <b>Perfecly Visible</b>:
 <ul>
     <li>Every part of the design clearly separates from the background.</li>
-    <li>You can hardly imagine a better background for the design in terms of visibility.</li>
+    <li>The design is easy to grasp quickly and completely even in a small preview.</li>
     <li>The background-color would be a great choice for displaying the design in the marketplace-search.</li>
 </ul>
